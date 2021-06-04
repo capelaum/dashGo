4fcbeb4b1a5771c5cb4bf66f890227c65939bb7d
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-06-04 17:32:10 -0300

    React Query Mutations: Create User

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,3 +1,7 @@
+import React from "react";
+import { useRouter } from "next/router";
+import Head from "next/Head";
+import Link from "next/link";
 import {
   Box,
   Divider,
@@ -8,16 +12,16 @@ import {
   HStack,
   Button,
 } from "@chakra-ui/react";
-import React from "react";
-import Link from "next/link";
-import { useForm, SubmitHandler } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useMutation } from "react-query";
 
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { Input } from "../../components/Form/Input";
-import Head from "next/Head";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
 
 type CreateUserFormData = {
   email: string;
@@ -39,6 +43,25 @@ const createUserFormSchema = yup.object().shape({
 });
 
 export default function CreateUser() {
+  const router = useRouter();
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post("users", {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
+
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["users"]);
+      },
+    }
+  );
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema),
   });
@@ -46,9 +69,9 @@ export default function CreateUser() {
   const { errors } = formState;
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async data => {
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    await createUser.mutateAsync(data);
 
-    console.log(data);
+    router.push("/users");
   };
 
   return (
